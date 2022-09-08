7d05b63333c3405fe3dc54a97f13a46f95f44446
Kabaikuu <nyokabihope2000@gmail.com>
2022-09-08 14:01:52 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,36 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                                     
 
-/**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-char c;
+                                                                                                                                                       
+
+/**                                                                                                                                                    
+
+*main - print the string in the put function                                                                                                           
+
+*                                                                                                                                                      
+
+*Description: using the main function                                                                                                                  
+
+*this program prints "Programming is like building a multilingual puzzle"                                                                              
+
+*Return: 0                                                                                                                                             
+
+*/                                                                                                                                                     
+
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+
+char c;                                                                                                                                                
+
 int i;
 long li;
 long long lli;
 float f;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of a long int: %ld byte(s)\n", sizeof(lli));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of an int: %ld 4 byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld 4 byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld 8 byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld 4 byte(s)\n", sizeof(f));
 return (0);
 }
