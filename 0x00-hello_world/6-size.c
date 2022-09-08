6cfe5ab8f32b5dbc24a9032ac7dcce8a8fbef996
Kabaikuu <nyokabihope2000@gmail.com>
2022-09-08 14:27:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,33 +1,21 @@
-#include <stdio.h>                                                                                                                                     
+#include <stdio.h>
 
-                                                                                                                                                       
-
-/**                                                                                                                                                    
-
-*main - print the string in the put function                                                                                                           
-
-*                                                                                                                                                      
-
-*Description: using the main function                                                                                                                  
-
-*this program prints "Programming is like building a multilingual puzzle"                                                                              
-
-*Return: 0                                                                                                                                             
-
-*/                                                                                                                                                     
-
-int main(void)                                                                                                                                         
-
-{                                                                                                                                                      
-
-char c;                                                                                                                                                
-
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "Programming is like building  a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+char c;
 int i;
 long li;
 long long lli;
 float f;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of a char: %ld 1 byte(s)\n", sizeof(c));
 printf("Size of an int: %ld 4 byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld 4 byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld 8 byte(s)\n", sizeof(lli));
